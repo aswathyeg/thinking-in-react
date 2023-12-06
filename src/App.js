f1227da2b6c627223ca1717685cf8c46020edb02
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2023-12-06 15:48:13 +0530

    added components

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import FilterableproductTable from './components/FilterableproductTable';
 
 function App() {
   return (
     <div className="App">
-     
+     <FilterableproductTable/>
     </div>
   );
 }
---
+++ src/components/ProductCategoryRow.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const ProductCategoryRow = () => {
+  return (
+    <div>ProductCategoryRow</div>
+  )
+}
+
+export default ProductCategoryRow
