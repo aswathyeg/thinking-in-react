db23f74a13a1e04d58baa808488535f621875749
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2023-12-12 17:21:15 +0530

    updated

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,8 +1,25 @@
 import React from 'react'
 
-const SearchBar = () => {
+const SearchBar = (props) => {
   return (
-    <div>SearchBar</div>
+    <div>
+        <form className='form'>
+        <input type="text"
+    placeholder='Search'
+    value={props.filterText}
+    onChange={(e)=>props.setFilterText(e.target.value)}
+    />
+    <label>
+    <br></br>
+    <input type="checkbox"
+    checked={props.inStock}
+    onChange={(e)=>props.setInStock(e.target.checked)}
+    />
+    {' '}
+'Only show products in stock'
+</label>
+</form>
+    </div>
   )
 }
 
