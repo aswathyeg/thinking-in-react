a78b2772187a19d6e906699c7a6355a8191b35c6
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2023-12-12 17:21:03 +0530

    added forEach

--- src/components/ProductTable.js
+++ src/components/ProductTable.js
@@ -1,8 +1,53 @@
 import React from 'react'
+import ProductCategoryRow from './ProductCategoryRow'
+import ProductRow from './ProductRow'
 
-const ProductTable = () => {
+const ProductTable = (props) => {
+    const rows=[];
+    let lastCategory=null;
+    props.products.forEach((product) => {
+        if(
+            product.name.toLowerCase().indexOf(props.filterText.toLowerCase())===-1 //if not found same text
+            
+        )
+        
+    {return;//if found same text
+    }
+    if(props.inStock && !product.stocked){
+        return;
+
+    }
+    if(product.category!==lastCategory){
+        rows.push(
+            <ProductCategoryRow
+            category={product.category}
+            key= {product.category}/>
+        )
+    }
+    rows.push(
+        <ProductRow
+        product={product}
+        key={product.name}
+        
+        />
+    )
+    lastCategory=product.category;
+});
   return (
-    <div>ProductTable</div>
+  
+    <table>
+        <thead>
+            <tr>
+                <th>Name</th>
+                <th>Price</th>
+            </tr>
+        </thead>
+        <tbody>{rows}</tbody>
+    </table>
+        
+        
+
+  
   )
 }
 
