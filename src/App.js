53297e7eaa003ed2743bd99ed6b53c3177de1141
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2023-12-12 17:18:07 +0530

    added data

--- src/App.js
+++ src/App.js
@@ -3,9 +3,17 @@ import './App.css';
 import FilterableproductTable from './components/FilterableproductTable';
 
 function App() {
+  const PRODUCTS = [
+    {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
+    {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
+    {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
+    {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
+    {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
+    {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+  ];
   return (
     <div className="App">
-     <FilterableproductTable/>
+     <FilterableproductTable products={PRODUCTS}/>
     </div>
   );
 }
