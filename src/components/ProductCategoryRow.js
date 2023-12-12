4ca4437eaefd0b67a345b4a1f36026de67f85db5
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2023-12-12 17:19:38 +0530

    added jsx

--- src/components/ProductCategoryRow.js
+++ src/components/ProductCategoryRow.js
@@ -1,8 +1,12 @@
 import React from 'react'
 
-const ProductCategoryRow = () => {
+const ProductCategoryRow = (props) => {
   return (
-    <div>ProductCategoryRow</div>
+    <tr>
+        <th colSpan={2}>
+            {props.category}
+        </th>
+        </tr>
   )
 }
 
