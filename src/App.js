05a141b40dfcb58193e6310e6746efe07b3342cd
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-02-07 14:44:19 +0530

    updared as array

--- src/App.js
+++ src/App.js
@@ -1,19 +1,20 @@
 
 import './App.css';
-import FilterableproductTable from './components/FilterableproductTable';
+
+import ImageSlider from './components/ImageSlider';
 
 function App() {
-  const PRODUCTS = [
-    {category: "Fruits", price: "$1", stocked: true, name: "Apple"},
-    {category: "Fruits", price: "$1", stocked: true, name: "Dragonfruit"},
-    {category: "Fruits", price: "$2", stocked: false, name: "Passionfruit"},
-    {category: "Vegetables", price: "$2", stocked: true, name: "Spinach"},
-    {category: "Vegetables", price: "$4", stocked: false, name: "Pumpkin"},
-    {category: "Vegetables", price: "$1", stocked: true, name: "Peas"}
+  const data = [
+    "https://media.istockphoto.com/id/1382384282/photo/bangalore-or-bengaluru.jpg?s=612x612&w=0&k=20&c=6pxwL3JxNV2B_NZSLMZLhrSLqAbyCPlGuSZYKImpjKQ=",
+    "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
+    "https://images.unsplash.com/photo-1541675154750-0444c7d51e8e?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGJlbmdhbHVydXxlbnwwfHwwfHx8MA%3D%3D",
+    "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
+    
   ];
   return (
     <div className="App">
-     <FilterableproductTable products={PRODUCTS}/>
+     <ImageSlider data={data}/>
     </div>
   );
 }
