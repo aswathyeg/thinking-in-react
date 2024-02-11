47c679a974105d5d35540a01a7ac767142842d82
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-02-11 07:38:40 +0530

    updated

--- src/components/ImageSlider.js
+++ src/components/ImageSlider.js
@@ -1,18 +1,23 @@
 import React, { useState } from 'react'
 import './ImageSlider.css'
-const ImageSlider = ({data}) => {
+const ImageSlider = (props) => {
     const[index,setIndex]=useState(0);
+
     const handlePrev=()=>{
-setIndex(index-1)
+
+(index===0)?setIndex(props.data.length-1):
+ setIndex(index-1)
 
     }
     const handleNext=()=>{
-        setIndex(index+1)
+       setIndex((index+1)%(props.data.length))
+        
+
     }
   return (
     <div >
         <div className='images'>
-        <img src={data[index]} alt="sliderImages">      
+        <img src={props.data[index]} alt="sliderImages">      
         </img>
         </div>
         <button onClick={handlePrev}>prev</button>
