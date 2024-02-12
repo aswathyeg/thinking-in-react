fdd9579612aaabc64b96df2c71be72357842ec69
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-02-12 15:23:40 +0530

    added array

--- src/components/UpdateArray.js
+++ src/components/UpdateArray.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 const UpdateArray = () => {
+    const initialList=[
+        { id: 0, title: 'Big Bellies', seen: false },
+  { id: 1, title: 'Lunar Landscape', seen: false },
+  { id: 2, title: 'Terracotta Army', seen: true },
+    ]
   return (
     <div>UpdateArray</div>
   )
