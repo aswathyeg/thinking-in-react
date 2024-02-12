56a1bfa989b734c6ac9af26d8c9dad2e83f84850
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-02-12 15:20:09 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -1,20 +1,13 @@
 
 import './App.css';
 
-import ImageSlider from './components/ImageSlider';
+import ListExp from './components/UpdateObject';
 
 function App() {
-  const data = [
-    "https://media.istockphoto.com/id/1382384282/photo/bangalore-or-bengaluru.jpg?s=612x612&w=0&k=20&c=6pxwL3JxNV2B_NZSLMZLhrSLqAbyCPlGuSZYKImpjKQ=",
-    "https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_1280.jpg",
-    "https://images.unsplash.com/photo-1541675154750-0444c7d51e8e?q=80&w=1000&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTN8fGJlbmdhbHVydXxlbnwwfHwwfHx8MA%3D%3D",
-    "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    "https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500",
-    
-  ];
+ 
   return (
     <div className="App">
-     <ImageSlider data={data}/>
+     <ListExp/>
     </div>
   );
 }
