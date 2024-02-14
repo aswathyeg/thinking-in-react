6352eab49fae2d27da8353a57501ba85ee310e78
Aswathy E G <aswathy.eg.bijesh@gmail.com>
2024-02-14 18:08:53 +0530

    updated

--- src/components/UpdateArray.js
+++ src/components/UpdateArray.js
@@ -1,13 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const UpdateArray = () => {
-    const initialList=[
+    const [initialList,setInitialList]=useState([
         { id: 0, title: 'Big Bellies', seen: false },
   { id: 1, title: 'Lunar Landscape', seen: false },
   { id: 2, title: 'Terracotta Army', seen: true },
-    ]
+    ]);
+    // const handleClick=()=>{
+    //     setInitialList([...initialList,
+    //     initialList.seen=true])
+    // }
   return (
-    <div>UpdateArray</div>
+
+    <div>
+        {
+initialList.map((initial)=>{
+    return(
+        <ul>
+        <li key={initial.id} 
+        
+        >
+            <input type='checkbox' ></input>
+            {initial.title}
+            {/* onClick={handleClick} */}
+        </li>
+    </ul>
+    
+    )
+   
+   
+
+})
+        }
+        
+        
+        </div>
   )
 }
 
